e0ec7cd4da75217ef0d6893428303e5d0aca72e8
Vaibhav Khati <khativibhu@gmail.com>
2026-08-05 00:13:01 +0530

    initialization

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req,res) => {
+    res.send("Hello world");
+} );
+
+app.get("/health", (req,res) => {
+    res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+   console.log("Server is running on port 8081");
+});
