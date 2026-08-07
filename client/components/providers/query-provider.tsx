fcf6d010d2d467a7c023b80211d5e8809751ea63
Vaibhav Khati <khativibhu@gmail.com>
2026-08-07 14:45:56 +0530

    added routes,controllers,services,repositories for workspace features and source management

---
+++ client/components/providers/query-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from 'react'
+import { QueryClientProvider, QueryClient} from '@tanstack/react-query'
+
+const QueryProvider = ({children}: { children: React.ReactNode }) => {
+    const [queryClient] = useState(() => new QueryClient()) 
+
+    return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+    </QueryClientProvider>
+  )
+}
+
+export default QueryProvider
