47329e1e039990432037c8aba8abf3aed27d4ad9
Vaibhav Khati <khativibhu@gmail.com>
2026-08-08 20:25:45 +0530

    implemented indexing phase of rag pipeline

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,30 @@ import express from "express";
 import "dotenv/config";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import cors from "cors";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
+import { registerRoutes } from "./routes/index.js";
+import { inngest } from "./inngest/client.js";
+import { serve } from "inngest/express";
+import { functions } from "./inngest/index.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const clientUrl = process.env.CLIENT_URL ?? "http://localhost:3000";
+
+
+app.use(
+    cors({
+        origin: clientUrl,
+        credentials: true,
+    }),
+);
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
-// Mount express json middleware after Better Auth handler
-// or only apply it to routes that don't interact with Better Auth
+
 app.use(express.json());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req,res) => {
     res.send("Hello world");
@@ -18,6 +34,11 @@ app.get("/", (req,res) => {
 app.get("/health", (req,res) => {
     res.json({ status: "ok" });
 });
+
+
+registerRoutes(app);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
    console.log("Server is running on port 8081");
---
+++ server/src/routes/source.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/async-handler.js";
+import { bulkDeleteSources, createSource, deleteSource, getSource, importWebsite, importYoutube, listSources, uploadPdf } from "../controllers/source.controllers.js";
+import { uploadSinglePdf } from "../middleware/upload.middleware.js";
+
+export const sourceRoutes = Router({ mergeParams: true });
+
+sourceRoutes.post(
+    "/upload",
+    uploadSinglePdf,
+    asyncHandler(uploadPdf),
+);
+sourceRoutes.post("/import/website", asyncHandler(importWebsite));
+sourceRoutes.post("/import/youtube", asyncHandler(importYoutube));
+
+sourceRoutes.get("/", asyncHandler(listSources));
+sourceRoutes.post("/", asyncHandler(createSource));
+sourceRoutes.post("/bulk-delete", asyncHandler(bulkDeleteSources));
+sourceRoutes.get("/:sourceId", asyncHandler(getSource));
+sourceRoutes.delete("/:sourceId", asyncHandler(deleteSource));
